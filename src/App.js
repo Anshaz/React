ee54ac54db2a61aa10448680be17cee384eeddf7
anshaj <anshaj9elnino@gmail.com>
2020-03-29 21:49:55 +0200

    react components 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 import './App.css';
 
 
@@ -14,6 +14,8 @@ class App extends Component {
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
+
+                <Menu/>
             </div>
         );
     }
