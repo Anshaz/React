12cbac64ebb025ccad791369b59da428a82d677e
anshaj <anshaj9elnino@gmail.com>
2020-04-04 22:53:41 +0200

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -37,7 +37,7 @@ function RenderDish({ dish }) {
     }
 
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments === 1) {
         return (<div></div>);
     }
@@ -56,7 +56,7 @@ function RenderComments({ comments }) {
                     </li>
                 </ul>
             ))}
-            <CommentForm />
+            <CommentForm dishId={dishId} addComment={addComment}/>
         </div>
     );
 }
@@ -82,7 +82,11 @@ const DishDetail = (props) => {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}
+
+                        />
                     </div>
                 </div>
             </div>
@@ -118,8 +122,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
