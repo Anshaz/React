05d56e8b101a08fe938ebc3fc770f18e484f074f
anshaj <anshaj9elnino@gmail.com>
2020-04-04 14:44:22 +0200

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem
-} from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import {Card,CardImg, CardText,CardBody,CardTitle,Modal,ModalHeader,ModalBody,Breadcrumb,BreadcrumbItem,Button,Row,Col,Label} from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
+import { Link } from "react-router-dom";
+
+/*defining the validators*/
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+
+
+/* here on, we can see the use of functional components within the same jsx file followed by class component named "CommentForms" down below*/
 
 function RenderDish({ dish }) {
         if (dish == null)
@@ -31,29 +39,27 @@ function RenderDish({ dish }) {
 
 function RenderComments({ comments }) {
     if (comments === 1) {
-        return (<div></div>)
+        return (<div></div>);
     }
-    const commentPlaced = comments.map(comment => {
-        return (
-            <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>-- {comment.author},
+
+    else return (
+        <div className="col-12 col-md-12 m-1">
+            <h4>Comments</h4>
+            {comments.map(comment => (
+                <ul key={comment.id} className="list-unstyled">
+
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author},
                     {new Intl.DateTimeFormat('en-US', { day: '2-digit', month: 'short', year: 'numeric' }).format(new Date(comment.date))}
-                </p>
-            </li>
-        )
-    });
-
-    return (
-        <div className='col-12 col-md-12'>
-            <h3> Comments </h3>
-            <ol className='list-unstyled'>
-                {commentPlaced}
-            </ol>
-
+                        </p>
+                    </li>
+                </ul>
+            ))}
+            <CommentForm />
         </div>
     );
-    }
+}
 
 const DishDetail = (props) => {
 
@@ -86,12 +92,108 @@ const DishDetail = (props) => {
             return (
 
             <div></div>)
-                 
-        
-         
-        
-    
 
 }
 
+/*creating a class component named 'CommentForm' within DishDetailComponent which included actually functional component only. However, in this specific js files, both class and functional components are merged */
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    toggleModal() {
+
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+
+    }
+
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil" /> Submit Comment
+        </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handleSubmit}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={12}>
+                                    Rating
+                </Label>
+                                <Col md={{ size: 12 }}>
+                                    <Control.select model=".rating" name="rating"
+                                        className="form-control"
+                                    >
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={12}>
+                                    Your Name
+                </Label>
+                                <Col md={{ size: 12 }}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={12}>
+                                    Comment
+                </Label>
+                                <Col md={{ size: 12 }}>
+                                    <Control.textarea
+                                        model=".comment"
+                                        id="comment"
+                                        name="comment"
+                                        rows={5}
+                                        className="form-control"
+                                    />
+                                </Col>
+                            </Row>
+                            <Button type="submit" value="submit" color="primary">
+                                Submit
+              </Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
 export default DishDetail;
