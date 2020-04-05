0e4dc67bd95cda963e5c41a89bb247206d404230
anshaj <anshaj9elnino@gmail.com>
2020-04-05 22:24:30 +0200

    Fetch from server+handling errors

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import {Card,CardImg, CardText,CardBody,CardTitle,Modal,ModalHeader,ModalBody,Br
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 /*defining the validators*/
@@ -26,7 +27,7 @@ function RenderDish({ dish }) {
             return (
                 <div className='col-12 col-md-12 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle
-} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -11,21 +10,27 @@ function RenderCard({ item, isLoading, errMess }) {
             <Loading />
         );
     }
+    else if (item == null) {
+        return (
+          ( <div></div>)
+        );
+    }
+
     else if (errMess) {
         return (
             <h4>{errMess}</h4>
         );
 
     }
-    else
+        else
         return (
                 <Card>
-                    <CardImg src={item.image} alt={item.name} />
-                    <CardBody>
+                <CardImg top src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
                         <CardTitle>{item.name}</CardTitle>
                         {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
                         <CardText>{item.description}</CardText>
-                    </CardBody>
+                        </CardBody>
                 </Card>
             );
 
@@ -36,14 +41,18 @@ function Home(props) {
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading}
+                    <RenderCard item={props.dish}
+                        isLoading={props.dishesLoading}
                         errMess={props.dishesErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion}
+                        isLoading={props.promosLoading}
+                        errMess={props.promosErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                    <RenderCard item={props.leader}
+                        />
                 </div>
             </div>
         </div>
